38d96e1fd03c3f83c0945a1b44d36a4b28a10345
iSiddhi <siddhipatel096@gmail.com>
2025-12-12 19:33:51 +0530

    Initial commit from local JavaScript folder

---
+++ first.js
@@ -0,0 +1,57 @@
+console.log('This is the first JavaScript file.');
+console.log('hello i am siddhi');
+
+
+
+//variables
+name = 'Siddhi';
+console.log(name);
+// x=null;
+// console.log(x);
+y=undefined;
+console.log(y);
+isfollow=true;
+console.log(isfollow);
+
+
+//define variables
+var city='Pune';
+let country='India';
+const pi=3.14;
+console.log(city);
+console.log(country);
+console.log(pi);
+
+
+//data types
+let fname='Siddhi Patel';
+let age=21;
+let price=99.99;
+let  x=null;
+
+
+const student={
+    name:'Siddhi',
+    age:21,
+    isActive:true
+};
+console.log(student);      
+console.log(student.name); 
+student.age=student.age+1;
+console.log(student.age);
+
+
+//practice questions
+//1. Create a variable to store your name and print it on the console.
+
+const product={
+    name:"Pen",
+    rating :4,
+    offer:22,
+    price:270
+}
+console.log(product);
+
+
+//2.
+
